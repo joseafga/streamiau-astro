c5628475c1f1695ebbd9f7fc2711ccb4597607e5
José Almeida <jose.afga@gmail.com>
2026-07-29 01:42:39 -0300

    Fix escapes inside ECR tags

--- src/middleware.js
+++ src/middleware.js
@@ -2,9 +2,18 @@ export const onRequest = async (context, next) => {
   const { request, url, logger } = context;
   const response = await next();
   const html = await response.text();
+  const escapes = {
+    "&amp;": "&",
+    "&lt;": "<",
+    "&gt;": ">",
+    "&#39;": "'",
+    "&quot;": '"',
+  };
 
   console.log("");
   const replaced = html.replace(/ECR%([\s\S]*?)%ECR/g, (match, captured) => {
+    captured = captured.replace(/&amp;|&lt;|&gt;|&#39;|&quot;/g, (tag) => escapes[tag]);
+
     const ecr_code = `<%${captured}%>`;
     logger.info(`Unescaped in ${url.pathname}: "${match}" → "${ecr_code}"`);
 
